d9abaa4ff9dfe8aa9b640b99ec9a280fd2763bb5
yiming lin <yiming.lin@outlook.com>
2016-11-12 01:33:39 +0800

    修改 appsettings.json 加入正式設定

--- wwwroot/app/item.js
+++ wwwroot/app/item.js
@@ -1,12 +1,2 @@
-"use strict";
-var Item = (function () {
-    function Item(Id, Title, Description) {
-        this.Id = Id;
-        this.Title = Title;
-        this.Description = Description;
-    }
-    return Item;
-}());
-exports.Item = Item;
-
+"use strict";var Item=function(){function Item(Id,Title,Description){this.Id=Id,this.Title=Title,this.Description=Description}return Item}();exports.Item=Item;
 //# sourceMappingURL=item.js.map
